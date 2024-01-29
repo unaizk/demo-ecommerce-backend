1fe87e307b82b409d365847418c3a7d0219d8bbe
root <root@Unaislaptop>
2024-01-29 23:36:17 +0530

    changes made

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors({ origin: "https://demo-ecommerce-frontend.unaizk.com", credentials
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/golbalSearch/products', express.static(path.join(__dirname, '../public/productImage')));
+app.use('/golbalSearch/products', express.static(path.join(__dirname, '/public/productImage')));
 
 
 app.use('/api/users', userRoutes);
